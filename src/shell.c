3715cc4cf1a2a8f6ee4533a6162f74099b3eaa07
sugarhush <hemansakkthivelm@gmail.com>
2026-07-18 00:03:34 +0530

    located type executable files

--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "shell.h"
 
 void initialize() {
@@ -32,13 +33,45 @@ void command_handling(char *command) {
 
     //type
     else if(strncmp(command,"type ",5)==0) {
-        if (strcmp(command+5,"echo") == 0 || strcmp(command+5,"exit") == 0 || strcmp(command+5,"type") == 0) {
-            printf("%s is a shell builtin\n", command+5);
+        char next_command[INPUT_SIZE];
+        strcpy(next_command, command+5);
+        if (strcmp(next_command,"echo") == 0 || strcmp(next_command,"exit") == 0 || strcmp(next_command,"type") == 0) {
+            printf("%s is a shell builtin\n", next_command);
         } else {
-            printf("%s: not found\n", command+5);
+            if (search_for_executable(next_command) != NULL) {
+                printf("%s is %s\n", next_command,search_for_executable(next_command));
+            } else {
+                printf("%s: not found\n", next_command);
+            }
         }
     }
     else {
         printf("%s: command not found\n", command);
     }
+}
+
+char* search_for_executable(const char* filename) {
+    char* path_env = getenv("PATH");
+    if (!path_env) {
+        return NULL;
+    }
+
+    char* path_copy = strdup(path_env);
+    if (!path_copy) {
+        return NULL;
+    }
+
+    char full_path[FILE_PATH];
+    char* token = strtok(path_copy,DELIMITER);
+    while (token != NULL) {
+        snprintf(full_path, sizeof(full_path), "%s/%s", token, filename);
+        if (access(full_path, X_OK) == 0) {
+            free(path_copy);
+            return strdup(full_path);
+        }
+        token = strtok(NULL,DELIMITER);
+    }
+
+    free(path_copy);
+    return NULL;
 }
--- src/shell.h
+++ src/shell.h
@@ -2,9 +2,12 @@
 #define SHELL_H
 
 #define INPUT_SIZE 100
+#define FILE_PATH 1024
+#define DELIMITER ":"
 
 void repl();
 void initialize();
-void command_handling(char *command);
+void command_handling(char *);
+char* search_for_executable(const char*);
 
 #endif
